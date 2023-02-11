4cc4cde405408a673a229ec2eadaa73ba7a1e242
mshubitidze <59510085+mshubitidze@users.noreply.github.com>
2023-02-11 08:13:22 +0400

    auth sucks

--- firebase.ts
+++ firebase.ts
@@ -4,13 +4,14 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCxJeavIinC7xL5ZyKJKO6fZNChtyNqc9o",
   authDomain: "chatgpt-clone-da184.firebaseapp.com",
   projectId: "chatgpt-clone-da184",
   storageBucket: "chatgpt-clone-da184.appspot.com",
   messagingSenderId: "404745497864",
-  appId: "1:404745497864:web:8f4aad16e279c60c9a50bd"
+  appId: "1:404745497864:web:864267a71309375d9a50bd"
 };
 
 // Initialize Firebase
